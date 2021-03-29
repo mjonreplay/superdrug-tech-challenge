37f845e3b258b7194575a2798d128a1dad9c57cc
Matt <matthew@wearespectacular.com>
2021-03-29 02:07:25 +0100

    Added recipe completion function that completes cycle and resets machine

--- scripts/dist/main.js
+++ scripts/dist/main.js
@@ -27,33 +27,77 @@ var selectedRecipe = function selectedRecipe(e, recipe) {
   // Set active button
   var selected = e.currentTarget;
   Array.from(document.querySelectorAll('.sd-btn-primary')).forEach(function (el) {
-    el.classList.remove('active');
-  });
-  selected.classList.add('active'); // Reset display
+    el.classList.add('disabled');
+  }); // Reset display
 
   var displayEl = document.getElementById("sd-display");
   displayEl.innerHTML = ""; // Populate Display
 
   var recipeTitleEl = document.createElement("h2");
-  var recipeTitleName = document.createTextNode(recipe.name);
+  var recipeTitleName = document.createTextNode(recipe.name + " in progress...");
   recipeTitleEl.appendChild(recipeTitleName);
   displayEl.appendChild(recipeTitleEl); // Include Steps
 
   var recipeStepEl = document.createElement("p");
-  stepGenerator(recipeStepEl, recipe.steps);
-};
+  stepGenerator(recipeStepEl, recipe);
+}; // Step Process 
 
-var stepGenerator = function stepGenerator(recipeStepEl, steps) {
+
+var stepGenerator = function stepGenerator(recipeStepEl, recipe) {
   var iterationTime = 1000;
-  steps.forEach(function (step, i) {
+  recipe.steps.forEach(function (step, i) {
     var displayEl = document.getElementById("sd-display");
     setTimeout(function () {
       recipeStepEl.innerHTML = "";
       var recipeStepName = document.createTextNode(step.name);
       recipeStepEl.appendChild(recipeStepName);
       displayEl.appendChild(recipeStepEl);
+      progressBar(step); // Last iteration
+
+      if (i === recipe.steps.length - 1) setTimeout(function () {
+        recipeComplete(recipe);
+      }, step.duration * 1000);
     }, iterationTime);
     iterationTime += step.duration * 1000;
   });
+}; // Display Progress Bar
+
+
+var progressBar = function progressBar(step) {
+  // Reset (Remove) Bar
+  var bar = document.getElementById("sd-progress-bar");
+  if (document.getElementById("sd-progress-bar")) bar.remove(); // Create Progress bar
+
+  var displayEl = document.getElementById("sd-display");
+  var progressBarEl = document.createElement("div");
+  progressBarEl.setAttribute("id", "sd-progress-bar"); // Set Duration
+
+  progressBarEl.style.transitionDuration = step.duration + "s";
+  displayEl.appendChild(progressBarEl).focus();
+  progressBarEl.classList.add('active');
+}; // Initialise Recipe Completion
+
+
+var recipeComplete = function recipeComplete(recipe) {
+  // Clear Display
+  var displayEl = document.getElementById("sd-display");
+  displayEl.innerHTML = ""; // Show thank you message
+
+  var completionMessageEl = document.createElement("h2");
+  var completionMessage = document.createTextNode("All done!");
+  completionMessageEl.appendChild(completionMessage);
+  var completionSubMessageEl = document.createElement("p");
+  var completionSubMessage = document.createTextNode("Please take your " + recipe.name + " and have a nice day!");
+  completionSubMessageEl.appendChild(completionSubMessage);
+  displayEl.appendChild(completionMessageEl);
+  displayEl.appendChild(completionSubMessageEl);
+  setTimeout(function () {
+    // Clear display after message
+    displayEl.innerHTML = ""; // Enable Buttons
+
+    Array.from(document.querySelectorAll('.sd-btn-primary')).forEach(function (el) {
+      el.classList.remove('disabled');
+    });
+  }, 5000);
 };
 //# sourceMappingURL=main.js.map
--- scripts/es6/main.js
+++ scripts/es6/main.js
@@ -23,9 +23,8 @@ let selectedRecipe = (e, recipe) => {
     // Set active button
     const selected = e.currentTarget;
     Array.from(document.querySelectorAll('.sd-btn-primary')).forEach((el) => {
-        el.classList.remove('active');
+        el.classList.add('disabled');
     });
-    selected.classList.add('active');
 
     // Reset display
     const displayEl = document.getElementById("sd-display");
@@ -33,27 +32,80 @@ let selectedRecipe = (e, recipe) => {
     
     // Populate Display
     const recipeTitleEl = document.createElement("h2");
-    const recipeTitleName = document.createTextNode(recipe.name);
+    const recipeTitleName = document.createTextNode(recipe.name+" in progress...");
     recipeTitleEl.appendChild(recipeTitleName);
     displayEl.appendChild(recipeTitleEl);
 
     // Include Steps
     const recipeStepEl = document.createElement("p");
-    stepGenerator(recipeStepEl, recipe.steps);
+    stepGenerator(recipeStepEl, recipe);
     
 }
 
-let stepGenerator = (recipeStepEl, steps) => {
+// Step Process 
+let stepGenerator = (recipeStepEl, recipe) => {
     let iterationTime = 1000;
-    steps.forEach((step, i) => {
+    recipe.steps.forEach((step, i) => {
         const displayEl = document.getElementById("sd-display");
         setTimeout(() => {
             recipeStepEl.innerHTML = "";
             const recipeStepName = document.createTextNode(step.name);
             recipeStepEl.appendChild(recipeStepName);
             displayEl.appendChild(recipeStepEl);
+            progressBar(step);
+            // Last iteration
+            if (i === recipe.steps.length - 1)
+                setTimeout(() => {
+                    recipeComplete(recipe);
+                }, step.duration * 1000);
         }, iterationTime);
         iterationTime += step.duration * 1000;
-        
     });
 };
+
+// Display Progress Bar
+let progressBar = (step) => {
+
+    // Reset (Remove) Bar
+    const bar = document.getElementById("sd-progress-bar");
+    if(document.getElementById("sd-progress-bar"))
+        bar.remove()
+
+    // Create Progress bar
+    const displayEl = document.getElementById("sd-display");
+    const progressBarEl = document.createElement("div");
+    progressBarEl.setAttribute("id", "sd-progress-bar");
+
+    // Set Duration
+    progressBarEl.style.transitionDuration = step.duration+"s";
+    displayEl.appendChild(progressBarEl).focus();
+    progressBarEl.classList.add('active');
+}
+
+// Initialise Recipe Completion
+let recipeComplete = (recipe) => {
+    
+    // Clear Display
+    const displayEl = document.getElementById("sd-display");
+    displayEl.innerHTML = "";
+
+    // Show thank you message
+    const completionMessageEl = document.createElement("h2");
+    const completionMessage = document.createTextNode("All done!");
+    completionMessageEl.appendChild(completionMessage);
+    const completionSubMessageEl = document.createElement("p");
+    const completionSubMessage = document.createTextNode("Please take your "+recipe.name+" and have a nice day!");
+    completionSubMessageEl.appendChild(completionSubMessage);
+    displayEl.appendChild(completionMessageEl);
+    displayEl.appendChild(completionSubMessageEl);
+
+    setTimeout(() => {
+        // Clear display after message
+        displayEl.innerHTML = "";
+
+        // Enable Buttons
+        Array.from(document.querySelectorAll('.sd-btn-primary')).forEach((el) => {
+            el.classList.remove('disabled');
+        });
+    }, 5000);
+}
